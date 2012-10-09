8f8784ba4ebcd8eb099c4e119a9a8d5a349efd61
Steve Zesch <stevezesch2@gmail.com>
2012-10-09 11:24:27 -0400

    Fixed fsf addresses. Closes #4.

--- src/mateconf-utils.h
+++ src/mateconf-utils.h
@@ -17,7 +17,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Street #330, Boston, MA 02111-1307, USA.
+ *  Foundation, Inc., 59 Temple Street #330, Boston, MA 02110-1301, USA.
  */
 
 /* eel-mateconf-extensions.h - Stuff to make MateConf easier to use.
@@ -36,8 +36,8 @@
 
    You should have received a copy of the GNU Library General Public
    License along with the Mate Library; see the file COPYING.LIB.  If not,
-   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.
+   write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+   Boston, MA 02110-1301, USA.
 
    Authors: Ramiro Estrugo <ramiro@eazel.com>
 */
