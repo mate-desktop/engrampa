mate-desktop/engrampa

commit 45deb61cce3acdf82b466a83013bbd72b30c1df9

zip: fixed -- position for the extraction command, added for list and test

src/fr-command-zip.c
@@ -190,6 +190,7 @@ fr_command_zip_list (FrCommand  *comm)
 	fr_process_begin_command (comm->process, "unzip");
 	fr_process_set_begin_func (comm->process, list__begin, comm);
 	fr_process_add_arg (comm->process, "-ZTs");
+	fr_process_add_arg (comm->process, "--");
 	fr_process_add_arg (comm->process, comm->filename);
 	fr_process_end_command (comm->process);
 	fr_process_start (comm->process);
@@ -254,6 +255,7 @@ fr_command_zip_add (FrCommand     *comm,
 
 	fr_process_add_arg (comm->process, comm->filename);
 	fr_process_add_arg (comm->process, "--");
+
 	for (scan = file_list; scan; scan = scan->next)
 		fr_process_add_arg (comm->process, scan->data);
 
@@ -277,6 +279,7 @@ fr_command_zip_delete (FrCommand  *comm,
 
 	fr_process_add_arg (comm->process, comm->filename);
 	fr_process_add_arg (comm->process, "--");
+
 	for (scan = file_list; scan; scan = scan->next) {
 		char *escaped;
 
@@ -322,6 +325,7 @@ fr_command_zip_extract (FrCommand  *comm,
 
 	fr_process_add_arg (comm->process, comm->filename);
 	fr_process_add_arg (comm->process, "--");
+
 	for (scan = file_list; scan; scan = scan->next) {
 		char *escaped;
 
@@ -340,6 +344,7 @@ fr_command_zip_test (FrCommand   *comm)
 	fr_process_begin_command (comm->process, "unzip");
 	fr_process_add_arg (comm->process, "-t");
 	add_password_arg (comm, comm->password);
+	fr_process_add_arg (comm->process, "--");
 	fr_process_add_arg (comm->process, comm->filename);
 	fr_process_end_command (comm->process);
 }

commit 0bf2354ffbe9c8fa7189b3373df7d95877978282

Fix zip file extract broken by 45deb61cce3acdf82b466a83013bbd72b30c1df9

src/fr-command-zip.c
@@ -323,8 +323,8 @@ fr_command_zip_extract (FrCommand  *comm,
 		fr_process_add_arg (comm->process, "-j");
 	add_password_arg (comm, comm->password);
 
-	fr_process_add_arg (comm->process, comm->filename);
 	fr_process_add_arg (comm->process, "--");
+	fr_process_add_arg (comm->process, comm->filename);
 
 	for (scan = file_list; scan; scan = scan->next) {
 		char *escaped;