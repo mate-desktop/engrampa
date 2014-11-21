f4e64e2f675057d23a94277fa103479549f862bc
infirit <infirit@gmail.com>
2014-11-21 15:43:10 +0100

    added a cancellable object to some GIO functions
    Based on FR commit: a323c11554785a9861fff4ae919e91c076babd82
    From: Paolo Bacchilega <paobac@src.gnome.org>

--- src/fr-archive.c
+++ src/fr-archive.c
@@ -1147,7 +1147,7 @@ load_local_archive (FrArchive  *archive,
 	FrCommand  *tmp_command;
 	const char *mime_type;
 
-	if (! g_file_query_exists (archive->file, NULL)) {
+	if (! g_file_query_exists (archive->file, archive->priv->cancellable)) {
 		fr_archive_action_completed (archive,
 					     FR_ACTION_LOADING_ARCHIVE,
 					     FR_PROC_ERROR_GENERIC,
@@ -1254,11 +1254,13 @@ copy_remote_file (FrArchive  *archive,
 {
 	XferData *xfer_data;
 
-	if (! g_file_query_exists (archive->file, NULL)) {
+	if (! g_file_query_exists (archive->file, archive->priv->cancellable)) {
 		GError *error;
+
 		error = g_error_new (G_IO_ERROR, G_IO_ERROR_NOT_FOUND, _("Archive not found"));
 		fr_archive_copy_done (archive, FR_ACTION_LOADING_ARCHIVE, error);
 		g_error_free (error);
+
 		return;
 	}
 
