15a6348f452358411ea30ac081fd6579b5a98c7b
ZenWalker <scow@riseup.net>
2015-10-09 22:59:00 +0200

    fixes #103
    fixed overwrite check when the files to be extracted are inside a folder

--- src/glib-utils.h
+++ src/glib-utils.h
@@ -80,6 +80,9 @@ char **            _g_strv_prepend               (char **str_array,
                                                   const char *str);
 gboolean           _g_strv_remove                (char **str_array,
                                                   const char *str);
+
+/* path filename */
+
 const char *        _g_path_get_file_name          (const char          *path);
 const char *        _g_path_get_base_name          (const char          *path,
 						    const char          *base_dir,
