66076cf781963cd1b781ba8e42d32efcf8fa9180
rbuj <robert.buj@gmail.com>
2019-08-24 12:21:31 +0200

    glib-utils: Remove unused function - g_ptr_array_copy
    closes https://github.com/mate-desktop/engrampa/issues/283

--- src/glib-utils.c
+++ src/glib-utils.c
@@ -568,22 +568,6 @@ get_time_string (time_t time)
 }
 
 
-GPtrArray *
-g_ptr_array_copy (GPtrArray *array)
-{
-	GPtrArray *new_array;
-
-	if (array == NULL)
-		return NULL;
-
-	new_array = g_ptr_array_sized_new (array->len);
-	memcpy (new_array->pdata, array->pdata, array->len * sizeof (gpointer));
-	new_array->len = array->len;
-
-	return new_array;
-}
-
-
 void
 g_ptr_array_free_full (GPtrArray *array,
                        GFunc      free_func,
