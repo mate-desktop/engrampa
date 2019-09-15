920724617e66bcab302c84f2d460cdcd965c14c8
rbuj <robert.buj@gmail.com>
2019-09-15 22:35:42 +0200

    Zstandard: Update MIME type
    MIME type is application/x-zstd:
    
      $ lorem-ipsum-generator -p 10000 -l -f plain > lorem.txt
      $ zstd lorem.txt
      $ file -b --mime-type lorem.txt.zst
      application/x-zstd
    
    Test:
    
      $ CFLAGS="-g -O0" ./autogen.sh --enable-magic --enable-packagekit --prefix=/usr && make && sudo make install

--- src/fr-init.c
+++ src/fr-init.c
@@ -110,8 +110,8 @@ FrMimeTypeDescription mime_type_desc[] = {
 	{ "application/x-xz",                                         ".xz",       N_("Xz (.xz)"), 0 },
 	{ "application/x-xz-compressed-tar",                          ".tar.xz",   N_("Tar compressed with xz (.tar.xz)"), 0 },
 	{ "application/x-zoo",                                        ".zoo",      N_("Zoo (.zoo)"), 0 },
+	{ "application/x-zstd",                                       ".zst",      N_("Zstandard (.zst)"), 0 },
 	{ "application/zip",                                          ".zip",      N_("Zip (.zip)"), 0 },
-	{ "application/zstd",                                         ".zst",      N_("Zstandard (.zst)"), 0 },
 	{ NULL, NULL, NULL, 0 }
 };
 
@@ -181,7 +181,7 @@ FrExtensionType file_ext_type[] = {
 	{ ".Z", "application/x-compress" },
 	{ ".zip", "application/zip" },
 	{ ".zoo", "application/x-zoo" },
-	{ ".zst", "application/zstd" },
+	{ ".zst", "application/x-zstd" },
 	{ NULL, NULL }
 };
 
